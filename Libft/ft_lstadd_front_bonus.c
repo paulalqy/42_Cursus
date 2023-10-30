fd8884a7bfeb98d2963a4ce6f217a4ac4d291bdd
Paula Lopez-Quintana Yep <palopez-@c2r18s5.42madrid.com>
2023-10-30 10:45:31 +0100

    25/10/2023

--- Libft/ft_lstadd_front_bonus.c
+++ Libft/ft_lstadd_front_bonus.c
@@ -1,33 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: palopez- <palopez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 17:01:12 by palopez-          #+#    #+#             */
-/*   Updated: 2023/10/25 13:25:33 by palopez-         ###   ########.fr       */
+/*   Created: 2023/10/25 12:55:22 by palopez-          #+#    #+#             */
+/*   Updated: 2023/10/25 13:29:08 by palopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*new_node;
+	new->next = *lst;
+	*lst = new;
+}
+/*int main()
+{
+	t_list	*first = ft_lstnew("hola");
+	t_list	*second = ft_lstnew("adios");
 
-	new_node = (t_list *) malloc (sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
-}/*
-int main()
-{
-	//char *str = (char *)malloc(sizeof(char) * 5);
-	t_list	*lst = ft_lstnew("HOLA");
-	printf("%s", (char *)lst->content);
-}	
-*/
+	t_list	*lst = NULL;
+	ft_lstadd_front(&lst, first);
+	printf("Primer nodo al principio: %s\n", (char *)lst->content);
+	ft_lstadd_front(&lst, second);
+	printf("Primer nodo despues: %s\n", (char *)lst->content);
+}*/
